005264b473a8c691bb463332cb3a7ff76e57610f
Raunak Chaurasia <raunakchaurasia402@gmail.com>
2025-10-04 23:35:48 +0530

    add count occurrence

---
+++ Array Easy/count occurrence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxFrequencyElements(int[] nums) {
+    int sum=0;
+    HashMap<Integer,Integer> map=new HashMap<>();
+    for(int i=0;i<nums.length;i++){
+        map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+    }  
+    int maxFreq = 0;
+    for (int freq : map.values()) {
+        maxFreq = Math.max(maxFreq, freq);
+    }
+    for (int freq : map.values()) {
+        if (freq == maxFreq) {
+            sum += freq;
+        }
+    }
+    return sum;
+    }
+}
